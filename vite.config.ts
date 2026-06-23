a083ef1efc04d81c9d9879f259476ed598a6406d
Mer <mer.estrella1@gmail.com>
2026-06-23 19:39:49 -0300

    Configurar despliegue a GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/po-builder/',
   plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: ['perchromic-karlee-aggregative.ngrok-free.dev'],
