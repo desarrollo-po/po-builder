1968aa12c06f1f1d12fb5e3809f9c42df876032a
unknown <juanv87@gmail.com>
2026-06-26 15:12:33 -0300

    Deploy settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/po-builder/',
   plugins: [react(), tailwindcss()],
   build: { chunkSizeWarningLimit: 600 },
   server: {
